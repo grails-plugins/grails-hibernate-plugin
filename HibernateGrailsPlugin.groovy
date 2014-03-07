grails-plugins/grails-hibernate-plugin

commit 12fa20cba174562841729be55f6a4de0644f4b01

GRAILS-10945 GRAILS-11133 activate HibernateJtaTransactionManagerAdapter by setting transactionManager so that JtaDelegate gets used in org.hibernate.engine.transaction.Isolater

src/groovy/grails/plugin/hibernate3/HibernatePluginSupport.groovy
@@ -247,6 +247,8 @@ Using Grails' default naming strategy: '${ImprovedNamingStrategy.name}'"""
                 hibernateProperties = ref("hibernateProperties$suffix")
 
                 grailsApplication = ref("grailsApplication", true)
+                
+                transactionManager = ref("transactionManager")
 
                 lobHandler = ref("lobHandlerDetector$suffix")
 

commit 8c461f86249850642f41d196aee2208fd7360b49

GRAILS-10945 GRAILS-11133 activate HibernateJtaTransactionManagerAdapter by setting transactionManager - use proxy to prevent circular bean references, fixes GRAILS-11189
This fixes problems caused by commit 12fa20cba174562841729be55f6a4de0644f4b01.

HibernateGrailsPlugin.groovy
@@ -40,6 +40,8 @@ class HibernateGrailsPlugin {
     def scm = [url: 'https://github.com/grails-plugins/grails-hibernate-plugin']
 
     def doWithSpring = HibernatePluginSupport.doWithSpring
+    
+    def doWithApplicationContext = HibernatePluginSupport.doWithApplicationContext
 
     def doWithDynamicMethods = HibernatePluginSupport.doWithDynamicMethods
 

src/groovy/grails/plugin/hibernate3/HibernatePluginSupport.groovy
@@ -46,6 +46,7 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader
 import org.springframework.context.ApplicationContext
 import org.springframework.jdbc.support.nativejdbc.CommonsDbcpNativeJdbcExtractor
 import org.springframework.orm.hibernate3.HibernateAccessor
+import org.springframework.transaction.PlatformTransactionManager
 
 /**
  * Implements the core parts of GORM.
@@ -248,8 +249,6 @@ Using Grails' default naming strategy: '${ImprovedNamingStrategy.name}'"""
                 hibernateProperties = ref("hibernateProperties$suffix")
 
                 grailsApplication = ref("grailsApplication", true)
-                
-                transactionManager = ref("transactionManager")
 
                 lobHandler = ref("lobHandlerDetector$suffix")
 
@@ -268,6 +267,8 @@ Using Grails' default naming strategy: '${ImprovedNamingStrategy.name}'"""
                                   'post-delete': eventTriggeringInterceptor]
 
                 hibernateEventListeners = ref('hibernateEventListeners')
+                
+                transactionManager = new PlatformTransactionManagerProxy()
             }
 
             if (grails.util.Environment.current.isReloadEnabled()) {
@@ -451,5 +452,14 @@ Using Grails' default naming strategy: '${ImprovedNamingStrategy.name}'"""
             return springConfig.getBeanDefinition("dataSource")
         }
         return null
+    }
+    
+    static doWithApplicationContext = { ApplicationContext ctx ->
+        PlatformTransactionManager transactionManager = ctx.getBean("transactionManager", PlatformTransactionManager)
+        ctx.getBeansOfType(ConfigurableLocalSessionFactoryBean).each { String beanName, ConfigurableLocalSessionFactoryBean sessionFactory ->
+            if(sessionFactory.transactionManager instanceof PlatformTransactionManagerProxy) {
+                sessionFactory.transactionManager.targetTransactionManager = transactionManager
+            }
+        }
     }
 }

src/groovy/grails/plugin/hibernate3/PlatformTransactionManagerProxy.java
@@ -1,0 +1,34 @@
+package grails.plugin.hibernate3;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+
+public class PlatformTransactionManagerProxy implements PlatformTransactionManager {
+    private PlatformTransactionManager targetTransactionManager;
+    
+    public PlatformTransactionManagerProxy() {
+        
+    }
+    
+    public TransactionStatus getTransaction(TransactionDefinition definition) throws TransactionException {
+        return targetTransactionManager.getTransaction(definition);
+    }
+
+    public void commit(TransactionStatus status) throws TransactionException {
+        targetTransactionManager.commit(status);
+    }
+
+    public void rollback(TransactionStatus status) throws TransactionException {
+        targetTransactionManager.rollback(status);
+    }
+
+    public PlatformTransactionManager getTargetTransactionManager() {
+        return targetTransactionManager;
+    }
+
+    public void setTargetTransactionManager(PlatformTransactionManager targetTransactionManager) {
+        this.targetTransactionManager = targetTransactionManager;
+    }
+}