895421709a845b4d9045349bfe0c7432bd95d59b
Lari Hotari <lhotari@gopivotal.com>
2014-03-24 08:38:17 +0200

    bump version to 3.6.10.12-SNAPSHOT

--- HibernateGrailsPlugin.groovy
+++ HibernateGrailsPlugin.groovy
@@ -27,7 +27,7 @@ class HibernateGrailsPlugin {
     def description = "Provides integration between Grails and Hibernate 3 through GORM"
 
     def grailsVersion = "2.3.2 > *"
-    def version = "3.6.10.11"
+    def version = "3.6.10.12-SNAPSHOT"
     def observe = ['domainClass']
     def loadAfter = ['controllers', 'domainClass']
     def watchedResources = ["file:./grails-app/conf/hibernate/**.xml"]
