fd72feb7d06a8b535813c57570f5476bf9a9c3ea
Lari Hotari <lhotari@pivotal.io>
2014-10-28 13:13:23 +0200

    use g-d-m 3.1.3.RELEASE

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,7 +29,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        String datastoreVersion = '3.1.3.BUILD-SNAPSHOT'
+        String datastoreVersion = '3.1.3.RELEASE'
         String hibernateVersion = '3.6.10.Final'
 
         compile "org.grails:grails-datastore-core:$datastoreVersion",
