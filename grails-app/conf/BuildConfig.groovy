b7bc745ac35301edc3c05b1612cad6c19d1350ad
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-18 20:52:46 -0400

    release 3.6.10.19 with g-d-m 3.1.4.RELEASE

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,6 @@
 if(System.getenv('TRAVIS_BRANCH')) {
     grails.project.repos.grailsCentral.username = System.getenv("GRAILS_CENTRAL_USERNAME")
-    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")    
+    grails.project.repos.grailsCentral.password = System.getenv("GRAILS_CENTRAL_PASSWORD")
 }
 
 
@@ -29,7 +29,7 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        String datastoreVersion = '3.1.4.BUILD-SNAPSHOT'
+        String datastoreVersion = '3.1.4.RELEASE'
         String hibernateVersion = '3.6.10.Final'
 
         compile "org.grails:grails-datastore-core:$datastoreVersion",
@@ -40,10 +40,10 @@ grails.project.dependency.resolution = {
                 exclude group:'org.springframework', name:'spring-core'
                 exclude group:'org.springframework', name:'spring-beans'
                 exclude group:'org.grails', name:'grails-bootstrap'
-                exclude group:'org.grails', name:'grails-core'                
-                exclude group:'org.grails', name:'grails-async'                 
+                exclude group:'org.grails', name:'grails-core'
+                exclude group:'org.grails', name:'grails-async'
         }
-                
+
         compile 'commons-collections:commons-collections:3.2.1'
         compile("org.hibernate:hibernate-core:$hibernateVersion") {
             exclude group:'commons-logging', name:'commons-logging'
@@ -55,11 +55,11 @@ grails.project.dependency.resolution = {
             exclude group:'org.hibernate.javax.persistence', name:'hibernate-jpa-2.0-api'
         }
         compile "javax.validation:validation-api:1.0.0.GA"
-        compile "org.hibernate:hibernate-validator:4.1.0.Final"        
+        compile "org.hibernate:hibernate-validator:4.1.0.Final"
         compile "org.hibernate:hibernate-entitymanager:$hibernateVersion", {
             exclude group:'javassist', name:'javassist'
             exclude group:'org.hibernate.javax.persistence', name:'hibernate-jpa-2.0-api'
-        } 
+        }
         compile "org.hibernate:hibernate-commons-annotations:3.2.0.Final"
         runtime "org.hibernate:hibernate-ehcache:$hibernateVersion", {
             exclude group: 'net.sf.ehcache', name: 'ehcache-core'
@@ -68,7 +68,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(':release:3.0.1', ':rest-client-builder:2.0.3') {
+        build(':release:3.1.0', ':rest-client-builder:2.1.0') {
             export = false
         }
 
